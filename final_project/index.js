7d7d006a74713648a69d7ed838ef1ccb15a8aea0
Vedaant Patel <vedupatel2011@gmail.com>
2024-09-08 20:04:50 -0400

    Implemented middleware for jwt

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,33 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    //Write the authenication mechanism here
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided' });
+    }
+
+    // Verify the token
+    jwt.verify(token, 'your-secret-key', function (err, decoded) {
+        if (err) {
+            return res.status(403).json({ message: 'Failed to authenticate token' });
+        }
+
+        // If the token is valid, store the decoded information (e.g., user info) in the request object
+        req.user = decoded;
+
+        // Proceed to the next middleware or route handler
+        next();
+    });
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
